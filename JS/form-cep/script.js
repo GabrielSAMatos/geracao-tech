b0548dd0da14413239a3aefd65240a9ecf1874f9
Gabriel Matos <matos7771@hotmail.com>
2024-11-15 18:07:35 -0300

    fetch aprendido

--- JS/form-cep/script.js
+++ JS/form-cep/script.js
@@ -3,7 +3,13 @@ cep.onkeyup = () => {
         fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
         .then(resposta => resposta.json())
         .then(resposta2 => {
-            console.log(resposta2)
+            rua.value = resposta2.logradouro;
+            bairro.value = resposta2.bairro;
+            cidade.value = resposta2.localidade;
+            estado.value = resposta2.estado;
+        })
+        .finally(() => {
+            alert("procedimento concluido")
         })
     }
 }
