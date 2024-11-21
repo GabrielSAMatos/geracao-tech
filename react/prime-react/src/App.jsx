8b05faff0f939b69af21d10e2d0962a300238d81
Gabriel Matos <matos7771@hotmail.com>
2024-11-21 10:11:54 -0300

    vendo prime react

---
+++ react/prime-react/src/App.jsx
@@ -0,0 +1,13 @@
+import "primereact/resources/themes/arya-blue/theme.css"
+import "primeflex/primeflex.css"
+import "primeicons/primeicons.css"
+
+const App = () => {
+  return ( 
+    <>
+    
+    </>
+   );
+}
+ 
+export default App;
