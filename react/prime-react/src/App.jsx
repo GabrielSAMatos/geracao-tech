13162b4458b98afc670f04e61a868bc71cdb23fe
Gabriel Matos <matos7771@hotmail.com>
2024-11-27 12:49:11 -0300

    agora é possível pegar os dados tanto de login como senha

--- react/prime-react/src/App.jsx
+++ react/prime-react/src/App.jsx
@@ -1,9 +1,6 @@
 import "primereact/resources/themes/lara-light-blue/theme.css"
 import "primeflex/primeflex.css"
 import "primeicons/primeicons.css"
-
-
-import { Button } from 'primereact/button';
 import Paths from "./routes/Paths";
         
 
--- react/prime-react/src/pages/Login.jsx
+++ react/prime-react/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { IconField } from 'primereact/iconfield';
 import { InputIcon } from 'primereact/inputicon';
 import { Button } from 'primereact/button';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
         
 
 
@@ -10,10 +11,16 @@ const Login = () => {
 
     const [showPassword, setShowPassword] = useState(false);
 
+    const {register, handleSubmit} = useForm();
+
+    function logar(dados){
+        console.log(dados);
+    }
+
     return ( 
         <>
             <div className='bg-blue-500 h-screen flex align-items-center justify-content-center px-3'>
-                <form className='col-12 md:col-3 surface-0 p-3 border-round-md'>
+                <form onSubmit={handleSubmit(logar)} className='col-12 md:col-3 surface-0 p-3 border-round-md'>
                     <h3 className='text-center text-4xl'>Welcome!</h3>
                     <label htmlFor="email" className='block uppercase font-bold text-sm mb-1'>Email</label>
                     <InputText
@@ -21,6 +28,7 @@ const Login = () => {
                     type='email'
                     placeholder='email@email.com'
                     className='mb-3 w-full'
+                    {...register('email', {required: true})}
                     />
 
                     <label htmlFor="password" className='block uppercase font-bold text-sm mb-1'>Password</label>
@@ -34,6 +42,8 @@ const Login = () => {
                                 id='password'
                                 placeholder='**********'
                                 className='w-full'
+                                {...register('password', {required: true})}
+
                                 type={showPassword ? 'text':'password'}
                             />
                         </IconField>
