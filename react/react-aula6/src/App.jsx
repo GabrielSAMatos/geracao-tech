14b94138f14fefe741537f803964eee8f30324be
Gabriel Matos <matos7771@hotmail.com>
2024-12-09 15:49:28 -0300

    Itens adicionados com a api fakestore, todos ja estilizados

--- react/react-aula6/src/App.jsx
+++ react/react-aula6/src/App.jsx
@@ -1,18 +1,9 @@
+import Paths from "./routes/Paths";
+
 const App = () => {
   
   return ( 
-    <>
-      <section>
-        <h1>Lista de Produtos</h1>
-        <ul className="flex">
-          <li className="col-12 md:col-3 bg-red-500">produto1</li>
-          <li className="col-12 md:col-3 bg-red-500">produto2</li>
-          <li className="col-12 md:col-3 bg-red-500">produto3</li>
-          <li className="col-12 md:col-3 bg-red-500">produto4</li>
-          <li className="col-12 md:col-3 bg-red-500">produto5</li>
-        </ul>
-      </section>
-    </>
+      <Paths/>
    );
 }
  
