31de39b6bc5009a7c1222e36ddc71ae20ad77fd7
Gabriel Matos <matos7771@hotmail.com>
2024-12-10 15:55:21 -0300

    introducao a POO

---
+++ backend/nodejs-1/routes/products.js
@@ -0,0 +1,31 @@
+function listProducts(){
+    const products = [
+        {
+            id: 1,
+            name: 'Mouse',
+            price: '10.00'
+        },
+        {
+            id: 2,
+            name: 'Keyboard',
+            price: '14.97'
+        }
+    ];
+    return products;
+}
+
+function addProducts(){
+
+}
+
+function removeProducts(){
+    
+}
+
+function editProducts(){
+    
+}
+
+
+
+module.exports = listProducts;
--- backend/nodejs-1/server.js
+++ backend/nodejs-1/server.js
@@ -1,7 +1,10 @@
 const { createServer } = require('node:http');
+const listProducts = require('./routes/products');
 
 const host = 'localhost';
 const port = 3000;
+
+
 
 const app = createServer((request, response) => {
 
@@ -14,9 +17,33 @@ const app = createServer((request, response) => {
         return response.end("NodeJS");
     }
 
+
     if(url === '/products'){
+
+        const data = listProducts();
+
+        response.writeHead(200, { 'Content-type': 'application/json'});
+        return response.end(JSON.stringify(data));
+    }
+
+    if(url === '/products/add'){
+
+        products.push({
+            id: 3,
+            name: 'Monitor',
+            price: 1000.00
+        })
+
         response.writeHead(200, { 'Content-type': 'text/plain'});
-        return response.end("Products");
+        return response.end("Product added successfully");
+    }
+
+    if(url === '/products/remove'){
+
+        products.pop()
+
+        response.writeHead(200, { 'Content-type': 'text/plain'});
+        return response.end("Product removed successfully");
     }
 
     response.writeHead(404, { 'Content-type': 'text/plain'});
